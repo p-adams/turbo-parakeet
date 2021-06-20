c385a394025e0f4dbab5e720ceaf2ac74bb48825
p-adams <ghusiyy@gmail.com>
2021-06-20 16:05:47 -0700

    create README.md in generated output directory

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,4 @@
 import * as semver from "https://deno.land/x/semver/mod.ts";
-/* 
-proc buildReadMe(packageJsonObj: JsonNode): string =
-    let name = packageJsonObj["name"].getStr()
-    let version = packageJsonObj["version"].getStr()
-    let readMe = &"""
-    ## Project
-        {name}
-    ### Version
-        {version}
-    """
-    result = readMe.unindent
-
-
-
-
-proc scanProject*(): string = 
-    #echo commandLineParams()
-    # hardcode single project path for dev
-    setCurrentDir("../../temp/react-app")
-    let rootDir = getCurrentDir()
-    if fileExists("package.json") == false:
-        result = "package.json not found"
-        return
-    for kind, path in walkDir(rootDir):
-        let pathSplit = splitPath(path)
-        if pathSplit.tail == "package.json":
-            let parsedPackageJson = parseFile(pathSplit.tail)
-            let readMe = buildReadMe(parsedPackageJson)
-            createDir(".turbo_parakeet")
-            setCurrentDir(".turbo_parakeet")
-            writeFile("README.md", readMe)
-            result = "View .turbo_parakeet directory in project root to view output."
-            return result
-
-
-*/
 
 export interface PackageJsonData {
   name: string;
@@ -75,6 +39,23 @@ async function main() {
       console.error("invalid package.json");
       return;
     }
+    // write README.md to generated output dir
+    Deno.mkdir(".turbo_parakeet")
+      .then(() => {
+        Deno.chdir(".turbo_parakeet");
+        Deno.writeTextFile("./README.md", buildReadmeFromPkgJson(pkgJson))
+          .then(() => {
+            console.log(
+              "View .turbo_parakeet directory in project root to view output."
+            );
+          })
+          .catch((error) => {
+            console.error(error.message);
+          });
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
   }
 }
 main();
